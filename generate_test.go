29f881238c7a85312064cfd665d3d2c0f3800f1f
Patrick Debois <Patrick.Debois@jedi.be>
2016-08-06 20:53:23 +0200

    some more generate tests

--- generate_test.go
+++ generate_test.go
@@ -71,9 +71,73 @@ func TestGroupByVirtualHost_virtualhostsenvvar(t *testing.T) {
 	}
 
 	if _, ok := group["www"]; ok {
-		//do something here
+		if len(group["www"]) != 1 {
+			t.Error("Expected only one item in the www group")
+		}
 	} else {
 		t.Error("Expected www group to be available")
+	}
+
+}
+
+func TestGroupByVirtualHost_twosamevirtualhostsenvvar(t *testing.T) {
+
+	ecsWatchInfoItem := &EcsWatchInfoItem{
+		Environment: map[string]string{
+			"VIRTUAL_HOST": "www",
+		},
+	}
+
+	var ecsWatchInfo EcsWatchInfo
+	ecsWatchInfo = append(ecsWatchInfo, *ecsWatchInfoItem)
+	ecsWatchInfo = append(ecsWatchInfo, *ecsWatchInfoItem)
+
+	group := groupByVirtualHost(ecsWatchInfo)
+
+	if len(group) != 1 {
+		t.Error("Expected 1 group got ", len(group))
+	}
+
+	if _, ok := group["www"]; ok {
+		if len(group["www"]) != 2 {
+			t.Error("Expected two item2 in the www group")
+		}
+	} else {
+		t.Error("Expected www group to be available")
+	}
+
+}
+
+func TestGroupByVirtualHost_twodifferentvirtualhostsenvvar(t *testing.T) {
+
+	ecsWatchInfoItem1 := &EcsWatchInfoItem{
+		Environment: map[string]string{
+			"VIRTUAL_HOST": "www1",
+		},
+	}
+
+	ecsWatchInfoItem2 := &EcsWatchInfoItem{
+		Environment: map[string]string{
+			"VIRTUAL_HOST": "www2",
+		},
+	}
+
+	var ecsWatchInfo EcsWatchInfo
+	ecsWatchInfo = append(ecsWatchInfo, *ecsWatchInfoItem1)
+	ecsWatchInfo = append(ecsWatchInfo, *ecsWatchInfoItem2)
+
+	group := groupByVirtualHost(ecsWatchInfo)
+
+	if len(group) != 2 {
+		t.Error("Expected 1 group got ", len(group))
+	}
+
+	if _, ok := group["www1"]; ok {
+		if len(group["www1"]) != 1 {
+			t.Error("Expected one item in the www1 group")
+		}
+	} else {
+		t.Error("Expected www1 group to be available")
 	}
 
 }
